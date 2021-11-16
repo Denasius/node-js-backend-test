af86242971ada89142be36aaa340b0853ee7c809
Denis <dev@webernetic.by>
2021-11-16 09:19:47 +0300

    added error hadler

--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -1,9 +1,17 @@
+const ApiError = require('../error/ApiError');
 class UserController {
     async registration(req, res) {}
 
     async login(req, res) {}
 
-    async check(req, res) {}
+    async check(req, res, next) {
+        // const {id} = req.query;
+        // if (!id) {
+        //     return next(ApiError.badRequest('Не передан ID'));
+        // }
+
+        res.json(req);
+    }
 }
 
 module.exports = new UserController()
--- server/index.js
+++ server/index.js
@@ -1,24 +1,27 @@
 require('dotenv').config();
 const express = require('express');
 const sequelize = require('./db');
-const models = require('./models/models')
+const models = require('./models/models');
 const cors = require('cors');
 const router = require('./routes/index');
+const errorHandler = require('./middleware/ErrorHandlingMiddleware');
 
-const PORT = process.env.PORT || 5000
+const PORT = process.env.PORT || 5000;
 const app = express();
 
 app.use(cors());
 app.use(express.json());
 app.use('/api', router);
 
+app.use(errorHandler);
+
 const start = async () => {
     try {
-        await sequelize.authenticate()
-        await sequelize.sync()
+        await sequelize.authenticate();
+        await sequelize.sync();
         app.listen(PORT, () => {`Server started at port ${PORT}`})
     } catch (e) {
         console.log('ERROR: ' + e)
     }
-}
+};
 start();
